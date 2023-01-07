90946227f1beb3810cc1047db5c7b9e3b9e0003e
Anas Ahmed <anas.ahassan@yahoo.com>
2023-01-07 17:10:14 +0200

    Delete Assets/Scenes/php directory

--- Assets/Scenes/php/Delete .php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connection.php');
-$whereFiled = $_POST['whereField'];
-$whereCondtion= $_POST['whereCondition'];
-$sql= "delete from users where".$whereFiled."='".$whereCondtion."'";
-$result = mysqli_query($connect,$sql);
-?>
--- Assets/Scenes/php/connection.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$host = "localhost" ;
-$username = "id20078403_club_management";
-$password = "DdnUm_QFk)rM)<3%";
-$database = "id20078403_club_management_system";
-
-$link = mysqli_connect($host, $username, $password,$database);
-?>
